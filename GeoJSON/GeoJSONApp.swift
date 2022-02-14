d631989a35a518d7bf160f20e8ed7b4cd9c6326c
Marek Roslik <marek.roslik@gmail.com>
2022-02-14 12:54:15 +0300

    Initial Commit

---
+++ GeoJSON/GeoJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoJSONApp.swift
+//  GeoJSON
+//
+//  Created by Marek Roslik on 14.02.22.
+//
+
+import SwiftUI
+
+@main
+struct GeoJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
